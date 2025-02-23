a468b4855d77aa3382df648e59780d8da2abe3a9
Momento <Momento@gmail.com>
2025-02-23 21:55:13 +0300

    Добавлен decrypt_vigenere()

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -7,7 +7,21 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ""
+    keyword = keyword.lower()
+    key_length = len(keyword)
+
+    for i, char in enumerate(plaintext):
+        shift = ord(keyword[i % key_length]) - ord('a')
+        
+        if char.isupper():
+            new_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        elif char.islower():
+            new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            new_char = char 
+        
+        ciphertext += new_char
     return ciphertext
 
 
@@ -20,5 +34,19 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+    plaintext = ""
+    keyword = keyword.lower()
+    key_length = len(keyword)
+
+    for i, char in enumerate(ciphertext):
+        shift = ord(keyword[i % key_length]) - ord('a')
+        
+        if char.isupper():
+            new_char = chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+        elif char.islower():
+            new_char = chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            new_char = char 
+        
+        plaintext += new_char
     return plaintext
